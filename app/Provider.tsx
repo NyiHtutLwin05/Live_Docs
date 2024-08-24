52ba7aeeb2ad09b52f615391132a21f83def48fe
James <winleiei@Wins-MacBook-Air.local>
2024-08-24 13:55:46 +0630

    collaborative editor room with liveblocks

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,13 +1,20 @@
 "use client";
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import {
   ClientSideSuspense,
   LiveblocksProvider,
 } from "@liveblocks/react/suspense";
-import { ReactNode } from "react";
+import { ReactNode, use } from "react";
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+        return users;
+      }}
+      authEndpoint="/api/liveblocks-auth"
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
